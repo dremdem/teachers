9a778575c9df01e7ca2283391990110239ca27d4
Vladimir Yakovenko <vyakovenko@particle.one>
2021-07-24 20:24:48 +0300

    add supersimple list

--- directory/views.py
+++ directory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from directory.models import Teacher
+
 # Create your views here.
+
+def index(request):
+    context = {"teachers": Teacher.objects.all()}
+    return render(request, 'index.html', context=context)
