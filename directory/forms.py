7720b95956f9b467cab40c08ea6e21892af95710
Vladimir Yakovenko <vyakovenko@particle.one>
2021-07-24 21:12:12 +0300

    add conditions

---
+++ directory/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from directory.models import Subject
+
+
+class SearchForm(forms.Form):
+    first_letter_of_last_name = forms.CharField(required=False,
+                                                label='First letter of Last Name')
+    subject = forms.ChoiceField(label="Subject", required=False,
+                                choices=[(subject, subject) for
+                                         subject in
+                                         Subject.objects.values_list(
+                                             "subject_name",
+                                             flat=True)])
--- directory/views.py
+++ directory/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
-from directory.models import Teacher
+from directory.models import Teacher, Subject
+from directory.forms import SearchForm
+
 
 # Create your views here.
 
 def index(request):
-    context = {"teachers": Teacher.objects.all()}
+    form = SearchForm()
+    teachers = Teacher.objects.all()
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            conditions = {}
+            subject = form.cleaned_data['subject']
+            first_letter_of_last_name = form.cleaned_data['first_letter_of_last_name']
+            if subject:
+                conditions['_subjects__pk'] = subject
+            if first_letter_of_last_name:
+                conditions['last_name__startswith'] = first_letter_of_last_name
+            teachers = Teacher.objects.filter(**conditions)
+    context = {"teachers": teachers,
+               "form": form}
     return render(request, 'index.html', context=context)
